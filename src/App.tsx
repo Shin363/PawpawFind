79c02c297a911c4e8ea219c1a3d636922464eb15
신주현 <wngus4595@gmail.com>
2026-08-08 23:32:52 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>PawpawFind</h1>
+      <p>초기 프론트엔드 세팅이 완료되었습니다.</p>
+    </main>
+  )
+}
+
+export default App
